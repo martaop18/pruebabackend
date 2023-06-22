33dd36340aab44ea63b35950d5ff8225d6a38269
marceop <marta.op18@gmail.com>
2023-06-22 13:51:37 +0200

    Feat: role_id associated in user model

--- models/user.js
+++ models/user.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasOne(
+        models.Role, {
+          foreignKey: 'role_id'
+        }
+      )
     }
   }
   User.init({
